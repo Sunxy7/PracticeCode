066b2daf8f3685156e5c0be8ce242b351eb47882
Sunxy7 <sunxy7@foxmail.com>
2017-04-27 00:46:39 +0800

    新增选择排序

--- Sort/SelectSort.cpp
+++ Sort/SelectSort.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
+#include<stdlib.h>
+#include <time.h>
 using namespace std;
  
 void SelectSort(int arr[],int len)
 {
 	int min=0;//最小元素下标
 	int max=len-1;//最大元素下标
-	int i=1;
-	while (min < max)
+	int i=0;
+	while (min <= max)
 	{
 		i=min+1;
-		while (i>min && i<max )
+		while (i>=min && i<=max )
 		{
+			if (arr[min]>arr[max])
+			{
+				swap(arr[min],arr[max]);
+			}
 			if(arr[i]<arr[min])
 			{
 				swap(arr[i],arr[min]);
@@ -24,14 +30,12 @@ void SelectSort(int arr[],int len)
 		min++;
 		max--;
 	}
-		
-
 }
 
 int main()
 {
 	int i=0;
-	int arr[10]={36,14,9,2,10,1,51,3,27,4};
+	int arr[10]={6,4,9,2,0,1,5,3,7,8};
 	int len =sizeof(arr)/sizeof(arr[0]);
 	SelectSort(arr,len);
 	for (i=0;i<len;i++)
