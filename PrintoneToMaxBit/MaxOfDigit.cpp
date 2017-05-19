8b47a86777ada019d25bc67cb6d3195c9211b983
Sunxy7 <sunxy7@foxmail.com>
2017-05-20 00:03:58 +0800

    剑指offer-12-打印1到最大的n位数

--- PrintoneToMaxBit/MaxOfDigit.cpp
+++ PrintoneToMaxBit/MaxOfDigit.cpp
@@ -18,52 +18,89 @@ void PrintNum(char *number)
 	}
 	printf("\t");
 }
-bool Increment(char *number,int len)
+//bool Increment(char *number,int len)
+//{
+//	bool overflow = false;
+//	int flag= 0;
+//	int sum = 0;
+//	int i = 0;
+//	for (i = len - 1; i >= 0; i--)
+//	{
+//		int sum = number[i]-'0'+flag;
+//		if (i == len - 1)
+//		{
+//			sum++;
+//		}
+//		if (sum >= 10)
+//		{
+//			if (i == 0)//最高位产生进位，打印完成
+//			{
+//				overflow = true;
+//				break;
+//			}
+//			else
+//			{
+//				sum -= 10;
+//				flag=1;
+//				number[i] = sum + '0';
+//			}
+//		}
+//		else
+//		{
+//			number[i] = '0' + sum;
+//			break;
+//		}
+//			
+//	}
+//	return overflow;
+//}
+//void MaxOfDigit(int n)
+//{
+//	char *str = new char[n + 1];
+//	memset(str, '0', n);
+//	str[n] = '\0';
+//	while (!Increment(str,n))
+//	{
+//		PrintNum(str);
+//	}
+//	cout << endl;
+//  delete[] str; 
+//}
+void IncrementR(char *num, int len, int index)
 {
-	bool overflow = false;
-	int flag= 0;
-	int sum = 0;
 	int i = 0;
-	for (i = len - 1; i >= 0; i--)
+	if (len <= 0 || index < 0)
 	{
-		int sum = number[i]-'0'+flag;
-		if (i == len - 1)
-		{
-			sum++;
-		}
-		if (sum >= 10)
-		{
-			if (i == 0)//最高位产生进位，打印完成
-			{
-				overflow = true;
-				break;
-			}
-			else
-			{
-				sum -= 10;
-				flag=1;
-				number[i] = sum + '0';
-			}
-		}
-		else
-		{
-			number[i] = '0' + sum;
-			break;
-		}
-			
+		return;
 	}
-	return overflow;
-}
-void MaxOfDigit(int n)
-{
-	char *str = new char[n + 1];
-	memset(str, '0', n);
-	str[n] = '\0';
-	while (!Increment(str,n))
+	if (index == len - 1)
 	{
-		PrintNum(str);
+		PrintNum(num);
+		return;
+	}
+	for (i = 0; i < len; i++)
+	{
+		num[index+1] = i + '0';
+		IncrementR(num, len, index +1);
+	}
+}
+void MaxToDigitR(int n)
+{
+	if (n <= 0 )
+	{
+		return;
+	}
+	char *num = new char[n + 1];
+	memset(num, '0', n);
+	num[n] = '\0';
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		num[0] = i + '0';
+		IncrementR(num,n,0);
 	}
 	cout << endl;
+	delete[] num;
 }
 
 int main()
@@ -71,7 +108,8 @@ int main()
 	int n;
 	cout << "please enter a number:" << endl;
 	cin >> n;
-	MaxOfDigit(n);
+	//MaxOfDigit(n);
+	MaxToDigitR(n);
 	system("pause");
 	return 0;
 }
